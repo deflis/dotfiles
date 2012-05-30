0e4a12e1b4d0fde3e887e9d9c9f46a2de0a11c99
deflis <deflis@gmail.com>
2012-05-31 02:24:46 +0900

    セットアップ時にインストールするパッケージを増やした

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install zsh vim subversion openssh-server byobu
+sudo apt-get install zsh vim subversion openssh-server byobu build-essential git
 sudo chsh

4054aa079998ce2ab08dcb44972183b89a5d18ac
deflis <deflis@gmail.com>
2012-06-14 23:15:49 +0900

    gemでrehashしてくれるように嬉しい変更をする。

--- rbenv.zsh
+++ rbenv.zsh
@@ -1,3 +1,12 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 source ~/.rbenv/completions/rbenv.zsh
+
+function gem(){
+    $HOME/.rbenv/shims/gem $*
+    if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
+    then
+        rbenv rehash
+        rehash
+    fi
+}

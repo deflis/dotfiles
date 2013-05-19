773944f7ac2ecf5cdf8baee824da03dccfa7056a
deflis <deflis@gmail.com>
2013-05-19 16:48:55 +0900

    submoduleのsetup時にNeoBundleのmasterをチェックアウトするように修正

--- setup_unix.sh
+++ setup_unix.sh
@@ -2,9 +2,12 @@
 
 git submodule update --init
 
+cd ~/dotfiles/.vim/bundle/neobundle.vim
+git checkout master
+cd ~/dotfiles
+
 ln -snf ~/dotfiles/.zsh ~/
 ln -snf ~/dotfiles/.zshrc ~/
 ln -snf ~/dotfiles/.vim ~/
 ln -snf ~/dotfiles/.vimrc ~/
 
-cd ~/.vim/bundle/vimproc/

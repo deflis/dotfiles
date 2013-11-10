e6a246837359a16d45a6639727390ffa7ce7ad2f
deflis <deflis@gmail.com>
2013-11-10 22:23:36 +0900

    tmux.confを整理

--- setup_unix.sh
+++ setup_unix.sh
@@ -13,5 +13,5 @@ ln -snf ~/dotfiles/.zshrc ~/
 ln -snf ~/dotfiles/.vim ~/
 ln -snf ~/dotfiles/.vimrc ~/
 ln -snf ~/dotfiles/.gvimrc ~/
-ln -snf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
 

a856c135eaa9297a3a56c9cee30855f26d9cde9d
deflis <deflis@gmail.com>
2016-06-25 07:56:26 +0000

    fix setup script

--- setup_unix.sh
+++ setup_unix.sh
@@ -3,11 +3,11 @@
 git submodule update --init
 git submodule update
 
-ln -snf ~/dotfiles/.zsh ~/
+ln -snf ~/dotfiles/zsh ~/.zsh
 ln -snf ~/dotfiles/.zshrc ~/
-ln -snf ~/dotfiles/.vim ~/
+ln -snf ~/dotfiles/nvim ~/.vim
 ln -snf ~/dotfiles/.vimrc ~/
 ln -snf ~/dotfiles/.gvimrc ~/
 ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -snf ~/dotfiles/.tmux ~/.tmux
+ln -snf ~/dotfiles/tmux ~/.tmux
 

f594d6eb008efa480f7dcdd6f7108230627bf614
deflis <deflis@gmail.com>
2016-07-11 01:02:05 +0900

    compinit周りを修正

--- zsh/00-completion.zsh
+++ zsh/00-completion.zsh
@@ -1,6 +1,6 @@
 [ -d /usr/local/Library/Contributions ] && fpath=(/usr/local/Library/Contributions ${fpath})
 [ -d /usr/local/share/zsh-completions ] && fpath=(/usr/local/share/zsh-completions $fpath)
 fpath=(${HOME}/.zsh/functions/Completion ${fpath})
-autoload -U compinit
-compinit -u
+#autoload -U compinit
+#compinit -u
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' '+m:{A-Z}={a-z}'

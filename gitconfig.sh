633b5271c5cc5caec973f80ab1a164d01411d7fe
deflis <deflis@gmail.com>
2019-02-06 14:43:33 +0900

    git用の設定ファイルを念のために作成

---
+++ gitconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# 諸事情によりこのリポジトリだけdeflisにします
+git config --local user.name deflis
+git config --local user.email deflis@gmail.com
+
+# コミットを修正するとき
+# git commit --amend --author="deflis <deflis@gmail.com>"

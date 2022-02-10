7232c2ba407ed5dfba028114381815109c807fba
deflis <deflis@gmail.com>
2022-02-11 03:19:17 +0900

    どんな環境でも動くようにしておく

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+cd $HOME
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply deflis
 
 if !(type "brew" > /dev/null 2>&1) ; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+cd $(~/bin/chezmoi source-path)
 brew bundle

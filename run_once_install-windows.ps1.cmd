5e0bc9aca9f53e5c627ce70a79630f65ce1e5c3c
deflis <deflis@gmail.com>
2022-02-10 23:21:59 +0900

    Windowsの初期インストールスクリプト

---
+++ run_once_install-windows.ps1.cmd
@@ -0,0 +1,35 @@
+@set args=%*
+@pwsh -Command "iex((@('')*3+(cat '%~f0'|select -skip 3))-join[char]10)"
+@exit /b %ERRORLEVEL%
+
+# Scoop インストールしたりする initial script
+# PowerShell 7 がインストールされてる前提
+
+Function Test-CommandExists
+{
+  Param ($command)
+  $oldPreference = $ErrorActionPreference
+  $ErrorActionPreference = ‘stop’
+  try {if(Get-Command $command){“$command exists”}}
+  Catch {“$command does not exist”}
+  Finally {$ErrorActionPreference=$oldPreference}
+}
+
+If(!(Test-CommandExists scoop)) {
+  Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
+
+If(!(Test-CommandExists oh-my-posh)) {
+  scoop install oh-my-posh
+}
+If(!(Test-CommandExists ghq)) {
+  scoop install ghq
+}
+If(!(Test-CommandExists sudo)) {
+  scoop install sudo
+}
+If(!(Test-CommandExists git)) {
+  scoop install git
+}
+
+PowerShellGet\Install-Module posh-git -Scope CurrentUser

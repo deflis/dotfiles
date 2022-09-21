394e81ee7c3e0b2686259cd6f264d47226fdccc8
deflis <deflis@gmail.com>
2022-09-21 18:31:26 +0000

    Update private_dot_config/sheldon/plugins.toml Update private_dot_config/zsh/defer/compilation.zsh

--- private_dot_config/zsh/defer/compilation.zsh
+++ private_dot_config/zsh/defer/compilation.zsh
@@ -1,2 +1,4 @@
 [ -d /usr/local/Library/Contributions ] && fpath=(/usr/local/Library/Contributions ${fpath})
 [ -d /usr/local/share/zsh-completions ] && fpath=(/usr/local/share/zsh-completions $fpath)
+
+eval "$(chezmoi completion zsh)"

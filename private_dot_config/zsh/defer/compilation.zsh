cfdf5f8c4f16d4a414948fe372c7f5be2e2b66fc
deflis <deflis@gmail.com>
2022-09-21 18:43:30 +0000

    Update private_dot_config/zsh/defer/compilation.zsh

--- private_dot_config/zsh/defer/compilation.zsh
+++ private_dot_config/zsh/defer/compilation.zsh
@@ -1,4 +1,3 @@
 [ -d /usr/local/Library/Contributions ] && fpath=(/usr/local/Library/Contributions ${fpath})
 [ -d /usr/local/share/zsh-completions ] && fpath=(/usr/local/share/zsh-completions $fpath)
 
-eval "$(chezmoi completion zsh)"

4a932f58c6bd459d3598952ded2f5f817c104a77
deflis <deflis@gmail.com>
2022-09-21 18:22:03 +0000

    Update private_dot_config/zsh/defer/compilation.zsh Add private_dot_config/zsh/defer/shell.zsh

--- private_dot_config/zsh/defer/compilation.zsh
+++ private_dot_config/zsh/defer/compilation.zsh
@@ -1,1 +1,2 @@
-#
+[ -d /usr/local/Library/Contributions ] && fpath=(/usr/local/Library/Contributions ${fpath})
+[ -d /usr/local/share/zsh-completions ] && fpath=(/usr/local/share/zsh-completions $fpath)

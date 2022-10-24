9bd87dda6fb0a423b8821522f31512726d1430ee
deflis <deflis@gmail.com>
2022-10-24 20:55:55 +0900

    Update .config/zsh/defer/shell.zsh

--- private_dot_config/zsh/defer/shell.zsh
+++ private_dot_config/zsh/defer/shell.zsh
@@ -32,3 +32,11 @@ bindkey '^ ' autosuggest-accept
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
 
 bindkey '^R' anyframe-widget-put-history
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+

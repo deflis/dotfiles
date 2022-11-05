7376159cbd9c1ffa03833fb4d37b6280a409b5b8
deflis <deflis@gmail.com>
2022-11-06 06:01:16 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-if [ -e /opt/homebrew/bin/brew ] ; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
-fi
-if [ -e /home/linuxbrew/.linuxbrew/bin/brew ] ; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
-
-brew install chezmoi
-
 cd $HOME
-chezmoi init --apply deflis
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply deflis

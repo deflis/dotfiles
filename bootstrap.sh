8007dba55519a2a77ee3126f05acf10c98fb86a2
deflis <deflis@gmail.com>
2022-11-06 05:29:35 +0900

    Update bootstrap.sh Update affter_gitconfig.nu Update Brewfile.sh.cmd

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if [ -e /opt/homebrew/bin/brew ] ; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+if [ -e /home/linuxbrew/.linuxbrew/bin/brew ] ; then
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+brew install chezmoi
+
 cd $HOME
-sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply deflis
+chezmoi init --apply deflis

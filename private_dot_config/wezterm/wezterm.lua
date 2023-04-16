aa8d8a8984c3af9753d2c61da91fc63692f826ed
deflis <deflis@gmail.com>
2023-04-16 17:00:59 +0900

    Add .config/wezterm/wezterm.lua

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = 'OneHalfDark'
+config.window_background_opacity = 0.8
+
+
+-- and finally, return the configuration to wezterm
+return config

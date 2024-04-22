d04afcc18c9f28cb5af54ad26d4fde24969e178d
deflis <deflis@gmail.com>
2024-04-22 13:01:05 +0900

    Update .config/zsh/defer/.chezmoiremove Change attributes of .config/zsh/sync/rtx.zsh

--- private_dot_config/zsh/sync/rtx.zsh
+++ private_dot_config/zsh/sync/rtx.zsh
@@ -2,5 +2,6 @@ if type rtx &>/dev/null; then
   eval "$(rtx activate zsh)"
 fi
 if type mise &>/dev/null; then
+  eval "$(mise activate zsh --shims)" # should be first
   eval "$(mise activate zsh)"
 fi

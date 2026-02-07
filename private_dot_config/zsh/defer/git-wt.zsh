f8ed1b79f32d61832f1f81e56378d8d510c92791
deflis <deflis@gmail.com>
2026-02-07 23:43:06 +0900

    git-wt.zshにcdw関数を追加

--- private_dot_config/zsh/defer/git-wt.zsh
+++ private_dot_config/zsh/defer/git-wt.zsh
@@ -1,3 +1,10 @@
 if (( $+commands[git-wt] )); then
   eval "$(git wt --init zsh)"
+
+  function cdw() {
+    local selected=$(git-wt | fzf --height 40% --reverse | awk '{print $1}')
+    if [ -n "$selected" ]; then
+      cd "$selected"
+    fi
+  }
 fi

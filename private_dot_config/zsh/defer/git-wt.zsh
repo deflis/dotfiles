5a242b31cbd4f9cdb2a467c31fa68291840d1acd
deflis <deflis@gmail.com>
2026-02-07 23:50:47 +0900

    cdw関数の選択肢を修正し、ヘッダーを追加

--- private_dot_config/zsh/defer/git-wt.zsh
+++ private_dot_config/zsh/defer/git-wt.zsh
@@ -2,7 +2,7 @@ if (( $+commands[git-wt] )); then
   eval "$(git wt --init zsh)"
 
   function cdw() {
-    local selected=$(git-wt | fzf --height 40% --reverse | awk '{print $1}')
+    local selected=$(git-wt | tail -n +2 | fzf --height 40% --reverse --header "Select workspace" | awk '{print $2}')
     if [ -n "$selected" ]; then
       cd "$selected"
     fi

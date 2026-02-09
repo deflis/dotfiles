52b29890d259a8df195bb8908bec6fc5bdaf54f0
deflis <deflis@gmail.com>
2026-02-09 21:55:51 +0900

    compilation.zsh.tmplのgit-wtとcarapaceの設定を条件付きに変更
    - git-wtの初期化を条件付きで追加
    - carapaceの設定を条件付きで修正

--- private_dot_config/zsh/defer/git-wt.zsh
+++ private_dot_config/zsh/defer/git-wt.zsh
@@ -1,6 +1,4 @@
 if (( $+commands[git-wt] )); then
-  eval "$(git wt --init zsh)"
-
   function cdw() {
     local selected=$(git-wt | fzf --height 40% --header-lines=1 | awk '{if ($1 == "*") print $2; else print $1}')
     if [ -n "$selected" ]; then
